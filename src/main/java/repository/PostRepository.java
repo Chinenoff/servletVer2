12269d6193753d48e7d280303a13770fc7852d09
ChinenOFF <pro100cvv@gmail.com>
2022-09-19 21:21:50 +0300

    thread safety fix (post id)

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -8,6 +8,10 @@ import java.util.concurrent.ConcurrentHashMap;
 
 // Stub
 public class PostRepository {
+
+    private static Long counter= Long.valueOf(0);
+
+    //private final static Generator generator = new Generator();
     private final static Map<Long, Post> postMap = new ConcurrentHashMap<>();
 
     public List<Post> all() {
@@ -20,9 +24,13 @@ public class PostRepository {
     }
 
     public Post save(Post post) {
-        if (postMap.get(post.getId()) == null) postMap.put(post.getId(), post);
-        else {
-            long newID = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        System.out.println(post.getId());
+        if (!postMap.containsKey(post.getId())) {
+            /*var genId = getNewId();
+            post.setId(genId);*/
+            postMap.put(post.getId(), post);
+        } else {
+            long newID = getNewId();
             post.setId(newID);
             postMap.put(newID, post);
         }
@@ -32,5 +40,9 @@ public class PostRepository {
     public void removeById(long id) {
         if (postMap.get(id) == null) throw new NotFoundException();
         postMap.remove(id);
+    }
+
+    public static synchronized Long getNewId(){
+        return counter++;
     }
 }
